3e4752f4af11ebaea5f9eea3342e91eaf3de1bca
thanawat <tha_chookes@hotmail.com>
2017-01-09 02:43:36 +0700

    added new Pods :tada:

--- eat/ViewController2.swift
+++ eat/ViewController2.swift
@@ -7,11 +7,34 @@
 //
 
 import UIKit
-
+import M13Checkbox
 class ViewController2: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        checkBoxAll()
+    }
+    
+    func checkBoxAll () {
+        let checkBoxPork = M13Checkbox(frame: CGRect(x: 60, y: 50, width: 40.0, height: 40.0))
+        view.addSubview(checkBoxPork)
+        
+        let  porkLabel = UILabel(frame: CGRect(x: 110, y: 50, width: 40.0, height: 40.0))
+        porkLabel.textAlignment = .left
+        porkLabel.text = "Pork"
+        view.addSubview(porkLabel)
+        
+        let checkBoxBeef = M13Checkbox(frame: CGRect(x: 60, y: 100, width: 40.0, height: 40.0))
+        view.addSubview(checkBoxBeef)
+        
+        let beefLabel = UILabel(frame: CGRect(x: 110, y: 100, width: 40.0, height: 40.0))
+        beefLabel.textAlignment = .left
+        beefLabel.text = "Beef"
+        view.addSubview(beefLabel)
+        
+        let checkBoxChicken = M13Checkbox(frame: CGRect(x: 60, y: 150, width: 40.0, height: 40.0))
+        view.addSubview(checkBoxChicken)
+        
     }
     
 }
